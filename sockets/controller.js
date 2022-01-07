122e4688c2752945f370e6cce0b8938f0585f68d
Alejandro2097 <alejo12381@hotmail.es>
2022-01-06 22:08:58 -0500

    hotfix

--- sockets/controller.js
+++ sockets/controller.js
@@ -1,13 +1,22 @@
 const { Socket } = require("socket.io");
 const { comprobarJWT } = require("../helpers");
+const { ChatMensajes } = require('../models');
 
-const socketController = async(socket = new Socket()) =>{
+const chatMensajes = new ChatMensajes();
+const socketController = async(socket = new Socket(), io) =>{
     // console.log('cliente conectado ' , socket.id);
     const usuario = await comprobarJWT(socket.handshake.headers['x-token']);
     if(!usuario){
         return socket.disconnect();
     }
-    console.log('Se conecto ', usuario.nombre);
+    
+    //Agregar al usuario conectado 
+    chatMensajes.conectarUsuario(usuario);
+    io.emit('usuarios-activos', chatMensajes.usuariosArr);
+    //Limpiar cuando un usuario se desconecta
+    socket.on('disconect', () =>{
+        chatMensajes.desconectarUsuario(usuario.id);
+    });
 } 
 
 module.exports = {
